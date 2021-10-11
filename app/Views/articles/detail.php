ec20802664ae76569d56439ffca63f0b5f7b7a31
Filbert Mangiri <filbert.mangiri@student.umn.ac.id>
2021-10-12 04:18:02 +0700

    Update

--- app/Views/articles/detail.php
+++ app/Views/articles/detail.php
@@ -1,5 +1,19 @@
 <?= $this->extend('layout/template'); ?>
 
+<?= $this->section('styles'); ?>
+<style>
+	.circle {
+		border-radius: 50%;
+		overflow: hidden;
+		width: 100%;
+		height: 100%;
+		border: 8px solid rgba(255, 255, 255, 0.7);
+		top: 72px;
+		transition: all .3s;
+	}
+</style>
+<?= $this->endSection(); ?>
+
 <?= $this->section('content'); ?>
 <div class="container" style="margin: auto;">
 	<div class="row">
@@ -37,14 +51,19 @@
 			$accountModel = new AccountModel();
 
 			foreach ($comments as $key) : ?>
-				<div class="container" style="width:50%">
-					<div class="col">
-						<?php $currAccount = $accountModel->accountGet($key['account_id']); ?>
-						<img src="<?= base_url('images/profiles/' . $currAccount['profile_picture']); ?>" alt="<?= $currAccount['username']; ?>'s Profile" class="img-fluid">
-					</div>
-					<div class="col">
-						<h4><?= $currAccount['first_name'] . ' ' . $currAccount['last_name']; ?></h4>
-						<p><?= $key['comment_text']; ?></p>
+				<div class="container">
+					<div class="row" style="width:80%;">
+						<span class="border border-primary border-5">
+							<div class="col circle" style="float: left;width:30%;">
+								<?php $currAccount = $accountModel->accountGet($key['account_id']); ?>
+								<img src="<?= base_url('images/profiles/' . $currAccount['profile_picture']); ?>" alt="<?= $currAccount['username']; ?>'s Profile" class="img-fluid">
+							</div>
+
+							<div class="col" style="float:left;width:50%;padding-top:10px;padding-left:10px;">
+								<h4><?= $currAccount['first_name'] . ' ' . $currAccount['last_name']; ?></h4>
+								<p><?= $key['comment_text']; ?></p>
+							</div>
+						</span>
 					</div>
 				</div>
 			<?php endforeach; ?>
